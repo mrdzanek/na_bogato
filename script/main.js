2d033d0e5b3b651f0813ab7773cc375bca54a246
Mirosław Rdzanek <rdzanek.miroslaw@gmail.com>
2021-04-10 18:48:04 +0200

    skeleton 2

--- script/main.js
+++ script/main.js
@@ -1,19 +1,54 @@
 // TODO
 
 Vue.component('car-view', {
-    props: ['car'],
+    props: ['cars'],
     data: function () {
-        return {
-        }
+        this.cars = [
+            {
+                model: 'Auris',
+                engine: '1.4',
+                extras: ['skórzana tapicerka', 'zajebiste alusy'],
+            },
+            {
+                model: 'Land Cruiser',
+                engine: '3.2',
+                extras: ['skórzana tapicerka', 'zajebiste alusy'],
+            }
+        ]
+        return this.cars;
     },
     template: `
- <div>Model: {{car.model}} 
-  <br> Silnik: {{car.engine}} 
-  <br> Opcje: {{car.extras}} 
-`
+    <div>
+        <p>Widok samochodów: </p>
+        <div v-for="car in cars"> 
+            Model: {{car.model}} 
+            <br> Silnik: {{car.engine}} 
+            <br> Opcje: {{car.extras}} 
+        </div>
+    </div>
+    `
 });
-var app = new Vue({
-    el: '#app',
-    data: {
-    }
-})
+var app;
+window.onload = function () {
+    app = new Vue({
+        el: '#app',
+        data: {
+            cars: [
+                {
+                    model: 'Auris',
+                    engine: '1.4',
+                    extras: ['skórzana tapicerka', 'zajebiste alusy'],
+                },
+                {
+                    model: 'Land Cruiser',
+                    engine: '3.2',
+                    extras: ['skórzana tapicerka', 'zajebiste alusy'],
+                }
+            ]
+        },
+        mounted: function () {
+            // axios.get('http://mockaroo/API_OD_MICHALA')
+            //     .then(result => this.cars = result)
+        },
+    });
+}
